5d68245361a0babe90e557346670b8224ed9456e
Ethan Liaw <ethan.liaw@everykey.com>
2021-02-15 18:00:08 -0500

    updating date/time again

--- app/src/main/java/com/example/assignment1/TimeService.java
+++ app/src/main/java/com/example/assignment1/TimeService.java
@@ -47,9 +47,9 @@ public class TimeService extends Service {
     public String getTime(){
         Calendar calendar = Calendar.getInstance();
         StringBuilder time = new StringBuilder();
-        time.append(Integer.toString(calendar.HOUR_OF_DAY));
+        time.append(Integer.toString(calendar.get(Calendar.HOUR_OF_DAY)));
         time.append(":");
-        time.append(Integer.toString(Calendar.MINUTE));
+        time.append(Integer.toString(calendar.get(Calendar.MINUTE)));
         return time.toString();
     }
 
